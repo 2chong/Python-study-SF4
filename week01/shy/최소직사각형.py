4a7ce2967510e93d47485bf81424237b5166aae2
shy0628 <serrayun@gmail.com>
2026-08-18 17:20:19 +0900

    Update 최소직사각형.py

--- week01/shy/최소직사각형.py
+++ week01/shy/최소직사각형.py
@@ -1,3 +1,8 @@
+'''
+[level 1] 최소직사각형 - 86491
+https://school.programmers.co.kr/learn/courses/30/lessons/86491
+'''
+
 def solution(sizes):
     newsizes = [sorted(i) for i in sizes]
     width = max(i[1] for i in newsizes)
